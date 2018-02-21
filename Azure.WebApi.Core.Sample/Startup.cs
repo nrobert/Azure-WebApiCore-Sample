4320171483e57aef7c2169bb6914b62281644553
Nicolas Robert <nicolas.robert@cellenza.com>
2018-02-21 23:47:22 +0100

    Including Swashbuckle - Basic setup

--- Azure.WebApi.Core.Sample/Startup.cs
+++ Azure.WebApi.Core.Sample/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Azure.WebApi.Core.Sample
 {
@@ -24,6 +19,11 @@ namespace Azure.WebApi.Core.Sample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "AZUG FR Sample API - .Net Core 2.0", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +35,12 @@ namespace Azure.WebApi.Core.Sample
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AZUG FR Sample API - .Net Core 2.0");
+            });
         }
     }
 }
